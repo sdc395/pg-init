89e387d393278d01cd2c3c2b21714c1f4d65fe2c
Simon <sdc395@users.noreply.github.com>
2025-10-28 17:02:29 +0000

    fix: typo

--- init-db.sh
+++ init-db.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 INIT_DIR="${SCRIPT_DIR}/init-db.d"
 
-echo "intialising database..."
+echo "initialising database..."
 
 find "$INIT_DIR" -type f -iname '*.sql' -print0 | sort -z | while read -d $'\0' file
 do
